6792102a584b9ecdf295fe4541411fc03f097ebf
santhosh-reddy750 <47923076+santhosh-reddy750@users.noreply.github.com>
2020-12-04 12:18:23 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./app2.css";
+ 
+const home = () => {
+    return (
+        
+       <div  className="home2" >
+           <h1>Recipe Search App</h1>
+          
+       </div>
+      
+    );
+}
+ 
+export default home;
