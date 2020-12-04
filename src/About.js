f65b2616a1dbb9bcb2cc9bce60f2e9c37c48cf50
santhosh-reddy750 <47923076+santhosh-reddy750@users.noreply.github.com>
2020-12-04 12:20:23 +0530

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./app2.css";
+const About = () => {
+    return (
+       <div className="home2">
+          <h1>About US</h1>
+          <p className="pp">Recipe app is a digital cooking guide that helps food enthusiasts and
+            home cooks to discover, save and create best recipes across the globe.</p>
+          
+       </div>
+    );
+}
+ 
+export default About;
