ef821f0e281deada46dd827c878273fb5d001f04
santhosh-reddy750 <47923076+santhosh-reddy750@users.noreply.github.com>
2020-12-04 12:22:32 +0530

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+ 
+const Contact = () => {
+    return (
+        <div class="container">
+        <form>
+      
+          <label>First Name</label>
+          <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+      
+          <label>Last Name</label>
+          <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+      
+          <label>Country</label>
+          
+      
+          <label >Subject</label>
+          <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+      
+          <input type="submit" value="Submit"/>
+      
+        </form>
+      </div>
+    );
+}
+ 
+export default Contact;
