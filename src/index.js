0f34b3c57fea530f2e6cd9fb72dfa31e3b664966
santhosh-reddy750 <47923076+santhosh-reddy750@users.noreply.github.com>
2020-12-04 12:28:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App2 from "./app2";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App2 />, document.getElementById("root"));
